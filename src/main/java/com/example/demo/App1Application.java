825506359ccb364608252ff420c206d38b615c1c
yunngna <nayunkwon124@gmail.com>
2025-01-23 14:48:54 +0900

    first commit

---
+++ src/main/java/com/example/demo/App1Application.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class App1Application {
+	@GetMapping("/")
+	public String main() {
+		return "Docker app 1 >>>> hello";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(App1Application.class, args);
+	}
+
+}
